ca56bdd1cbf4c6bbf875849b64d6dee31b297ac7
IrynaKonovalowa <iri.moummi@gmail.com>
2024-05-23 12:12:38 +0200

    first commit

---
+++ Valik.jsx
@@ -0,0 +1,17 @@
+﻿function Valik(props){
+    const [state, setState] = React.useState({name: props.data.name, kind: props.data.kind, hobby: props.data.hobby, character: props.data.character, food: props.data.food, age: props.data.age, photos: props.data.photos});
+    
+    return ( 
+        <div class="Valik">
+          <h1>{state.name}</h1>
+          <div>
+             <p><span>Вид: </span>{state.kind}</p>
+             <p><span>Возраст: </span>{state.age}</p>
+             <p><span>Увлечения: </span>{state.hobby}</p>
+             <p><span>Характер: </span>{state.character}</p>
+             <p><span>В еде: </span>{state.food}</p>
+             <br></br>
+             <p><span>Фотографии: </span>{state.photos}</p>
+          </div>
+        </div>);
+    }
---
+++ ValikPhoto.jsx
@@ -0,0 +1,8 @@
+﻿function ValikPhoto(props){
+  const [photo, setName] = React.useState(props.data.photo);
+   
+    return ( 
+        <div>          
+            <img src={photo} alt="FotoValik" />            
+        </div>);
+    }
